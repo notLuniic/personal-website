f9160c4e70e70a6fccbc9775b283dd18e066500a
Oliver <ojfk2001@gmail.com>
2026-02-12 14:56:27 +1100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div 
+      className="bg-cover bg-center"
+      style={{ 
+        backgroundImage: "url('/background.png')",
+        minHeight: '100vh',
+        width: '100vw'
+      }}
+    >
+    </div>
+  )
+}
+
+export default App
